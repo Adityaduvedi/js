457019b22f9ebb9dcb538ed4c44f1e1ec54935d0
Adityaduvedi <duvediaditya987@gmail.com>
2024-08-03 17:48:54 +0000

    d

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12323
+let accountEmail = "dfg@fgh"
+var accountPassword = "rtrty"
+accountCity = "jaipur"
+
+accountId = 2
+console.table([accountId , accountCity ,accountEmail , accountPassword]);
+
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "aditya"
+let age = "10"
+let isLoggedIn = false
+
+
+\\Number => 2 to power 53
+\\BigInt
+\\String => ""
+\\Boolean=> true/false
+\\null = standalone value 
+\\undefined
+\\symabol => unique
+
+
+\\object
+
+console.log(typeof "undefined"); \\undefined
+console.log(typeof "null"); \\object
