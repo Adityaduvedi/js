0fe4fbfe62c7527ac9a9a0dd1931951288bfb381
Adityaduvedi <duvediaditya987@gmail.com>
2024-08-05 11:55:10 +0000

    container config done

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -1,12 +1,12 @@
 let score = "33"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // 33 => 33
 // "33abc" => NaN
@@ -14,13 +14,17 @@ console.log(valueInNumber);
 
 let isLoggedin = ""
 let booleanisloggedin = Boolean(isLoggedin)
-console.log(booleanisloggedin);
+//console.log(booleanisloggedin);
 
 // 0=> false                 ((((((//"inme mein kuch bhi likho" =>true))))))))
 // 1=> true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+ 
+ 
 
