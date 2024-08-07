aabd264f5b398b657bc5c58c8540a75bb2792fbe
Adityaduvedi <duvediaditya987@gmail.com>
2024-08-07 17:45:08 +0000

    datatype summary

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); 
+// console.log(null == 0);
+// console.log(null >= 0); (its because in comparison >= null will be converted to 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === when there are three these than it also checks the data types //
+
+console.log("2" === 2);
+
+
+
---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+// 7 types : string , number , boolean , null , undefined , symbol , BigInt
+
+// Reference (Non Primitive)
+
+// 3 types : array , object , functions 
+
+// array -
+// const heros = ["hero","shaktimaan"]
+
+// object -
+// let myObj = { 
+    Name = "aditya",
+    age = 11,
+}
+
+// functions -
+const myFunction = function(){
+    console.log("hello world");
+    
+}
